62fe70e08b6864bfc0c902d0c80f0b4236212670
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-04 22:18:16 +0530

    Hollow Square

---
+++ 09-pattern9.js
@@ -0,0 +1,11 @@
+for(let row=1;row<=5;row++){
+    for(let col=1;col<=5;col++){
+        if(row==1 || row==5 || col==1 || col==5)
+    {
+          process.stdout.write("* ")
+    } else if (col==2 || col==3 || col==4){
+        process.stdout.write("  ")
+    }
+}
+console.log();
+}
