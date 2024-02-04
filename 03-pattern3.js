2e742e56007f4a0d9723e699a6596ccb6fab291c
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-04 18:43:05 +0530

    5 to 1 decreasing stars pattern

---
+++ 03-pattern3.js
@@ -0,0 +1,17 @@
+let nst =5;
+
+for (row = 1; row <= 5; row++) {
+
+
+    // stars
+    for (st = 1; st<=nst; st++) {
+        process.stdout.write("* ")
+    }
+
+
+
+    // new line
+    console.log();
+
+    nst=nst-1
+}
