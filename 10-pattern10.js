e495eca01d581850b15b56a0efaabfe6fdd834f5
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-04 22:39:36 +0530

    hollow triangle

--- 10-pattern10.js
+++ 10-pattern10.js
@@ -1,11 +1,10 @@
 for(let row=1;row<=5;row++){
     for(let col=1;col<=5;col++){
-        if(row==5 ||col==1 )
+        if(row==5 || col==1 || col==2 && row==2 || col==3 && row==3 || col==4 && row==4)
     {
           process.stdout.write("* ")
-    } else if (col==2 || col==3 || col==4){
+    } else {
         process.stdout.write("  ")
-    } 
     }
-console.log();
+} console.log();
 }
