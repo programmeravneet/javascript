4185f5d778620d661f7b0dcc706d3b17a2b7a776
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-26 23:38:15 +0530

    buttons are working now

--- DOM/script5.js
+++ DOM/script5.js
@@ -1,2 +1,29 @@
 var btns = document.querySelectorAll('button')
-console.log(querySelectorAll());
+for(let i = 0;i<btns.length;i++){
+    btns[i].addEventListener('click',function(){
+        if(btns[i]==btns[0]){
+            btns[i].style.backgroundColor='red'
+            btns[i].style.color='white'
+            
+        }
+        else if(btns[i]==btns[1]){
+            btns[i].style.backgroundColor='green'
+            btns[i].style.color='white'
+            
+        }
+        else if(btns[i]==btns[2]){
+            btns[i].style.backgroundColor='yellow'
+            
+        }
+        else if(btns[i]==btns[3]){
+            btns[i].style.backgroundColor='pink'
+            
+        }
+        else if(btns[i]==btns[4]){
+            btns[i].style.backgroundColor='blue'
+            btns[i].style.color='white'
+            
+        }
+    })
+   
+}
