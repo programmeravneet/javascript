cb185f52954246da8c42a12b5e34d5ffef17d41e
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-29 21:03:23 +0530

    fetch

--- Taram/question4.js
+++ Taram/question4.js
@@ -1,20 +1,13 @@
+    let f = null
     fetch('https://randomuser.me/api/')
     .then(raw=>raw.json())
-    .then(res=>{
-        let firstName = document.querySelector('input')
-        firstName.innerHTML=res.results[0].name.first}
-        
-        )
-   
-     
-
-
-
-
-let heading1 = document.createElement('h1')
-document.querySelector('body').appendChild(heading1)
-heading1.innerHTML=firstName
-
+    .then(res=>{f=(res.results[0].name.first)
+        console.log(f);
+        let fname = document.querySelector('#fname')
+        fname.value=f
+    
+    }
+    )
  // append child then add data in the input
 
  // inner.HTML main input banaker usme bhej denge out put ko
--- Taram/swipe.js
+++ Taram/swipe.js
@@ -1,5 +1,5 @@
 let btn = document.querySelector('button')
-btn.addEventListener('click',function(){
+btn.addEventListener('click',function(){ 
    
        let img1 = document.querySelector('#img1')
       img1.src="https://images.unsplash.com/photo-1682687982501-1e58ab814714?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
